d02ac15139eb14da6a09edacffc75a9b756d8ec3
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-21 09:15:27 +0530

    openapi codegen mvc test

--- codegen/codegen/src/main/java/com/myapp/impl/codegen/TestController.java
+++ codegen/codegen/src/main/java/com/myapp/impl/codegen/TestController.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 @RestController
 public class TestController implements EmployeesApi {
 
     @Override
     public ResponseEntity<List<Employee>> getEmployees() {
         return ResponseEntity.ok(List.of(new Employee().id(1).firstName("praj").lastName("s")));
+    }
+
+    @Override
+    public ResponseEntity<Employee> createEmployee(Employee employee) {
+        return ResponseEntity.status(CREATED).build();
     }
 }
