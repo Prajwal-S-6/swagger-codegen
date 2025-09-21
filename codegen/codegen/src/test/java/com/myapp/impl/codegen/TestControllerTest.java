0967ab22f8656731057a2a1bdda12d32f9aec004
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-21 09:15:30 +0530

    openapi codegen mvc test

--- codegen/codegen/src/test/java/com/myapp/impl/codegen/TestControllerTest.java
+++ codegen/codegen/src/test/java/com/myapp/impl/codegen/TestControllerTest.java
@@ -5,6 +5,7 @@ import com.myapp.impl.codegen.model.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -23,14 +25,26 @@ class TestControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     public void shouldReturn200WithOneEmployeeGetAllEmployees() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
+
         MvcResult result = mockMvc.perform(get("/employees")).andExpect(status().isOk()).andReturn();
 
         Employee[] employees = objectMapper.readValue(result.getResponse().getContentAsString(), Employee[].class);
         assertThat(employees.length).isEqualTo(1);
         assertThat(employees[0]).isEqualTo(new Employee().id(1).firstName("praj").lastName("s"));
+    }
+
+    @Test
+    public void shouldReturnCreatedForPOSTCallOnEmployee() throws Exception {
+        mockMvc.perform(post("/employees").contentType(MediaType.APPLICATION_JSON)
+                .content("{"
+                        + "\"id\": 1,"
+                        + "\"firstName\": \"string1\","
+                        + "\"lastName\": \"string2\""
+                        + "}")).andExpect(status().isCreated());
     }
 
 }
