3ed23f175f61a19b3dc496b3d95866ef7504456b
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-21 09:06:45 +0530

    openapi codegen mvc test

---
+++ codegen/codegen/src/test/java/com/myapp/impl/codegen/TestControllerTest.java
@@ -0,0 +1,36 @@
+package com.myapp.impl.codegen;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myapp.impl.codegen.model.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class TestControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn200WithOneEmployeeGetAllEmployees() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        MvcResult result = mockMvc.perform(get("/employees")).andExpect(status().isOk()).andReturn();
+
+        Employee[] employees = objectMapper.readValue(result.getResponse().getContentAsString(), Employee[].class);
+        assertThat(employees.length).isEqualTo(1);
+        assertThat(employees[0]).isEqualTo(new Employee().id(1).firstName("praj").lastName("s"));
+    }
+
+}
