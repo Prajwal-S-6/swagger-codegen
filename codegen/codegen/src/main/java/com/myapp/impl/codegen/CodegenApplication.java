c61f3264bc81307d295e5c27d8bd1b31a29b56eb
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-22 07:52:22 +0530

    openapi codegen angular

--- codegen/codegen/src/main/java/com/myapp/impl/codegen/CodegenApplication.java
+++ codegen/codegen/src/main/java/com/myapp/impl/codegen/CodegenApplication.java
@@ -3,7 +3,7 @@ package com.myapp.impl.codegen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class CodegenApplication {
 
 	public static void main(String[] args) {
